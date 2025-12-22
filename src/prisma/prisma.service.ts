a6f49ff1df1a859d448fb2c3b15dbb3a07649dc1
Dks2006-crypto <denis18kpina@gmail.com>
2025-12-22 21:39:42 +0300

    df

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,26 +1,13 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { ConfigService } from '@nestjs/config';
-import betterSqlite3 from 'better-sqlite3';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  constructor(configService: ConfigService) {
-    let url = configService.get<string>('DATABASE_URL');
-
-    if (!url) {
-      url = 'file:/app/data/dev.db';
-    }
-
-    const dbPath = url.replace(/^file:/, '');
-    const db = betterSqlite3(dbPath);
-    const adapter = new PrismaBetterSqlite3(db);
-
-    super({ adapter });
+  constructor() {
+    super();
   }
 
   async onModuleInit() {
